5b78964c1c7cd345bae2228aee5510a1fd7241ce
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 17:23:26 -0500

    Update page.tsx

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,1 +1,8 @@
-
+export default function ExplorePage(){
+  return (
+    <div>
+      <h3>Explore</h3>
+      <p>Placeholder explore page (flights, tours, attractions, etc.)</p>
+    </div>
+  )
+}
