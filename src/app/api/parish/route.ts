ece764697dea4727593aa99ba3baf28f4193a3ab
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 17:48:38 -0500

    Delete src/app/api/parish directory

--- src/app/api/parish/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
-
-export async function GET() {
-  const parishes = await prisma.parish.findMany({ include: { towns: true }});
-  return NextResponse.json(parishes);
-}
