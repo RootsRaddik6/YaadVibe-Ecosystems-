e2af41cf7507e6b200f038b2887455c8f8fc449a
RootsRaddik6 <korjamltd@gmail.com>
2025-11-24 14:46:02 -0500

    Create page.tsx

---
+++ src/app/map/page.tsx
@@ -0,0 +1,13 @@
+import MapContainer from "../(ui)/map/MapContainer";
+
+export default function MapPage() {
+  return (
+    <div style={{ padding: "24px" }}>
+      <h1 style={{ fontSize: "24px", marginBottom: "16px" }}>
+        Jamaica 3D Map (Coming Soon)
+      </h1>
+
+      <MapContainer />
+    </div>
+  );
+}
